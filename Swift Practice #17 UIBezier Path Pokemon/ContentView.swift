c592cbe98d49bdaf7caf9cba04f30cc15da5d945
DogpaChen <dogpa0130@gmail.com>
2021-06-28 14:00:01 +0800

    Initial Commit

---
+++ Swift Practice #17 UIBezier Path Pokemon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice #17 UIBezier Path Pokemon
+//
+//  Created by CHEN PEIHAO on 2021/6/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
